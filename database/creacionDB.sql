37e60ab4c89099c7fb422bb1a251a19d4cf373f2
aargudo1910 <aargudo@espol.edu.ec>
2021-02-01 23:06:05 -0500

    Corección errores database

--- database/creacionDB.sql
+++ database/creacionDB.sql
@@ -1,13 +1,12 @@
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema fundacion
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `fundacion` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE SCHEMA IF NOT EXISTS `fundacion` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
 USE `fundacion` ;
 
 -- -----------------------------------------------------
@@ -21,11 +20,11 @@ CREATE TABLE IF NOT EXISTS `fundacion`.`usuario` (
   `num_telefono` VARCHAR(45) NOT NULL,
   `ciudad` VARCHAR(45) NOT NULL,
   `direccion` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idusuario`),
-  UNIQUE INDEX `idusuario_UNIQUE` (`idusuario` ASC) VISIBLE)
+  PRIMARY KEY (`cedula`),
+  UNIQUE INDEX `cedula` (`cedula` ASC))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
 
 -- -----------------------------------------------------
 -- Table `fundacion`.`solicitud`
@@ -34,10 +33,10 @@ CREATE TABLE IF NOT EXISTS `fundacion`.`solicitud` (
   `idsolicitud` VARCHAR(45) NOT NULL, -- idsolicitud = cedula del usuario
   `estado` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idsolicitud`),
-  UNIQUE INDEX `idsolicitud_UNIQUE` (`idsolicitud` ASC) VISIBLE)
+  UNIQUE INDEX `idsolicitud_UNIQUE` (`idsolicitud` ASC))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
 
 
 -- -----------------------------------------------------
@@ -48,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `fundacion`.`mascotas` (
   `edad` VARCHAR(155) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
   `animal` VARCHAR(155) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
   `raza` VARCHAR(155) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL,
-  `color` VARCHAR(155) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL
+  `color` VARCHAR(155) CHARACTER SET 'utf8' COLLATE 'utf8_spanish_ci' NOT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
@@ -60,12 +59,11 @@ COLLATE = utf8_spanish_ci;
 CREATE TABLE IF NOT EXISTS `fundacion`.`administrador` (
   `idadministrador` INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`idadministrador`),
-  UNIQUE INDEX `idsolicitud_UNIQUE` (`idadministrador` ASC) VISIBLE)
+  UNIQUE INDEX `idsolicitud_UNIQUE` (`idadministrador` ASC))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_spanish_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
